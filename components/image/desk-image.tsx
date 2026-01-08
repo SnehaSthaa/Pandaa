8904e60c0d82ec5a7f6e0bf70196f9e97da5568e
SnehaSthaa <snehacrest1234@gmail.com>
2026-01-08 21:58:51 +0545

    updated

--- components/image/desk-image.tsx
+++ components/image/desk-image.tsx
@@ -30,7 +30,7 @@ const DeskImage = ({ image }: DeskImageProps) => {
     <>
       <div className="flex flex-row gap-4 lg:justify-between">
         <div className="flex flex-col gap-3">
-          <div className='mask-[no-repeat] mask-[url("/image/subtract1.png")] mask-size-[100%_100%] mask-[center_center]'>
+          <div className='mask-[url("/image/subtract1.png")] mask-size-[100%_100%] mask-[center_center] mask-no-repeat'>
             <AppImage
               src={image[0].img}
               alt="Image first"
